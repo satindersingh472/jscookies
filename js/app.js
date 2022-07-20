ef6f7fc6752d755c9604dab32b13de8244e5f9c1
satinder <satindersingh472@gmail.com>
2022-07-19 19:55:40 -0600

    cookies created removed and updated

--- js/app.js
+++ js/app.js
@@ -1,6 +1,25 @@
+function cookie_func(details) {
+    Cookies.set(`button_clicked`, `yes`);
+}
 let button = document.getElementById(`click_button`);
 button.addEventListener(`click`, cookie_func);
 
-function cookie_func(details){
-Cookies.set(`button_clicked` , `yes`);
+
+let cookie_value = Cookies.get(`button_clicked`);
+if (cookie_value !== undefined) {
+    document[`body`][`innerHTML`] += `<h1> congrats you baked a cookie</h1>`;
 }
+ else {
+    document[`body`].insertAdjacentHTML(`beforeEnd` , `<h1>no cookies</h1>`);
+}
+function close_cookie(details){
+    Cookies.remove(`button_clicked`);
+}
+let close_it = document.getElementById(`close_button`);
+close_it.addEventListener(`click`, close_cookie);
+
+function cookie_update(details){
+    Cookies.set(`button_clicked` , `updated`);
+}
+let new_cookie = document.getElementById(`update`);
+new_cookie.addEventListener(`click`,cookie_update);
